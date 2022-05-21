f615320c3c05764e62d0f36094783a0d1aeaf33b
Antoni Zyla <zyla.antoni@gmail.com>
2022-05-21 20:17:27 +0100

    Sending rays into the scene

--- main.cpp
+++ main.cpp
@@ -2,24 +2,47 @@
 
 #include "vec3.h"
 #include "colour.h"
+#include "ray.h"
+
+colour ray_colour(const ray& r);
 
 int main() {
 
-    //image file format
-    const int image_width = 256 ;
-    const int image_height = 256;
+    //Image
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width/aspect_ratio);
 
-    //render base image to screen
-    // header of file
+    //Camera
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0,0,0);
+    auto horizontal = vec3(viewport_width,0,0);
+    auto vertical = vec3(0,viewport_height,0);
+    auto lower_left = origin   - horizontal/2 - vertical/2 - vec3(0,0,focal_length);
+
+    // Rendering
+    // file header
     std::cout << "P3\n" << image_width << " " << image_height << "\n255\n";
-
+    // pixel data
     for (int j = image_height-1; j >= 0; --j) {
-
         std::cerr  << "\r Rows Remaining: " << j << " "<< std::flush;
-
         for (int i = 0; i < image_width; ++i){
-            colour pixel_colour(double(i)/(image_width-1), double(j)/(image_height-1), 0.25);
-            write_colour(std::cout, pixel_colour);            }
+            auto u = double(i) /(image_width-1);
+            auto v = double(i) / (image_height-1);
+            ray r(origin,lower_left + u*horizontal + v*vertical -origin);
+            colour pixel_colour = ray_colour(r);
+            write_colour(std::cout, pixel_colour);
+            }
         }
         std::cerr << "\n Render Complete";
+}
+
+
+colour ray_colour(const ray& r){
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 *(unit_direction.y() + 1.0);
+    return (1.0-t)*colour(1.0,1.0,1.0) +t*colour(0.5,0.7,1.0);
 }
