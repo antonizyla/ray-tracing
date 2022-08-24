314b4e9c041a510c0afd4b653f8ed3d4f739b35d
Antoni Zyla <zyla.antoni@gmail.com>
2022-08-24 21:29:47 +0100

    Multithreading implemented, spawns a thread for each pixel, cost of spawining negligible compared to compute time at larger res and samples

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <ostream>
 #include <sstream>
+#include <thread>
+#include <mutex>
 
 #include "rt_header.h"
 #include "colour.h"
@@ -14,26 +16,39 @@ double hit_sphere(const point3 &center, double radius, const ray &ray);
 
 colour ray_colour(const ray &r, const hittable &world, int depth);
 
-int main() {
+void trace_rays(int i, int j, const int samples, const int image_width, const int image_height, const camera &cam,
+                const hittable_list &world,
+                const int max_depth, std::string &pixel);
+
+
+int main(int argc, char **argv) {
+
+    using namespace std::chrono_literals;
+
+    bool multithreaded = false;
+    if (argc >= 2) {
+        multithreaded = true;
+    }
+
     //Image
     const auto aspect_ratio = 1.5;
-    const int image_width = 300;
+    const int image_width = 600;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples = 50;
-    const int max_depth = 50;
+    const int samples = 500;
+    const int max_depth = 300;
 
     //World
-    auto world = random_scene();
+    const hittable_list world = random_scene();
 
     //Camera
     point3 lookfrom(13, 2, 3);
     point3 lookat(0, 0, 0);
     vec3 vup(0, 1, 0);
     auto dist_to_focus = 10;
-//    auto dist_to_focus = (lookfrom-lookat).length();
+//  auto dist_to_focus = (lookfrom-lookat).length();
     auto aperture = 0.1;
 
-    camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
+    const camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
     // Files
     std::ofstream file("../Image.ppm");
@@ -43,24 +58,28 @@ int main() {
     auto start = std::chrono::steady_clock::now();
     // size of each pixel will be 12 bytes as a string
     // doing everything at once
+
     std::string image[image_width][image_height];
+
     for (int i = 0; i < image_width; i++) {
         for (int j = 0; j < image_height; j++) {
             image[i][j].resize(12);
-            colour pixel_colour(0, 0, 0);
-            for (int s = 0; s < samples; ++s) {
-                auto u = (i + random_double()) / (image_width - 1);
-                auto v = (j + random_double()) / (image_height - 1);
-                ray r = cam.get_ray(u, v);
-                pixel_colour += ray_colour(r, world, max_depth);
+            std::thread worker(trace_rays, i, j, samples, image_width, image_height, std::ref(cam), std::ref(world),
+                             max_depth, std::ref(image[i][j]));
+            if (multithreaded) {
+                worker.detach();
+            } else {
+                worker.join();
             }
-            image[i][j] = write_colour_str(pixel_colour, samples);
         }
-        std::cerr << "\r cols Remaining: " << image_width - i << "\n";
+        std::cerr << "\r Columns Remaining: " << image_width - i << "\n";
     }
+
+    std::this_thread::sleep_for (std::chrono::seconds(3));
 
     // ppm header
     oss << "P3\n" << image_width << " " << image_height << "\n255\n";
+    // image data
     for (int i = image_height - 1; i >= 0; --i) {
         for (int j = 0; j < image_width; j++) {
             oss << image[j][i] << std::endl;
@@ -108,4 +127,16 @@ colour ray_colour(const ray &r, const hittable &world, int depth) {
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * colour(1.0, 1.0, 1.0) + t * colour(0.5, 0.7, 1.0);
+}
+
+void trace_rays(int i, int j, const int samples, const int image_width, const int image_height, const camera &cam,
+                const hittable_list &world, const int max_depth, std::string &pixel) {
+    colour pixel_colour(0, 0, 0);
+    for (int s = 0; s < samples; ++s) {
+        auto u = (i + random_double()) / (image_width - 1);
+        auto v = (j + random_double()) / (image_height - 1);
+        ray r = cam.get_ray(u, v);
+        pixel_colour += ray_colour(r, world, max_depth);
+    }
+    pixel = write_colour_str(pixel_colour, samples);
 }
