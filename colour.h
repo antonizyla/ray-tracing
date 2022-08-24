b0d706c058ca2c26ce04e0eb59ced6b8ab310609
Antoni Zyla <zyla.antoni@gmail.com>
2022-08-24 15:32:16 +0100

    Halfway point for allowing multithreading

--- colour.h
+++ colour.h
@@ -7,8 +7,10 @@
 
 #include "vec3.h"
 #include <iostream>
+#include "vector"
 
-void write_colour(std::ostream &out, colour pixel_colour, int samples){
+
+void write_colour(std::ostream &out, colour pixel_colour, int samples) {
 
     auto r = pixel_colour.x();
     auto g = pixel_colour.y();
@@ -19,10 +21,26 @@ void write_colour(std::ostream &out, colour pixel_colour, int samples){
     g = sqrt(scale * g);
     b = sqrt(scale * b);
 
-    out << static_cast<int>(256*clamp(r, 0,0.999)) << " "
-            << static_cast<int>(256*clamp(g, 0,0.999)) << " "
-            << static_cast<int>(256*clamp(b, 0,0.999)) << std::endl;
+    out << static_cast<int>(256 * clamp(r, 0, 0.999)) << " "
+        << static_cast<int>(256 * clamp(g, 0, 0.999)) << " "
+        << static_cast<int>(256 * clamp(b, 0, 0.999)) << std::endl;
 }
 
+std::string write_colour_str(colour pixel_colour, int samples) {
+    auto r = pixel_colour.x();
+    auto g = pixel_colour.y();
+    auto b = pixel_colour.z();
+
+    auto scale = 1.0 / samples;
+    r = sqrt(scale * r);
+    g = sqrt(scale * g);
+    b = sqrt(scale * b);
+
+    std::ostringstream str;
+    str << static_cast<int>(256 * clamp(r, 0, 0.999)) << " "
+        << static_cast<int>(256 * clamp(g, 0, 0.999)) << " "
+        << static_cast<int>(256 * clamp(b, 0, 0.999));
+    return str.str();
+}
 
 #endif //RAY_TRACING_COLOUR_H
--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <ostream>
+#include <sstream>
 
 #include "rt_header.h"
 #include "colour.h"
@@ -8,27 +10,25 @@
 #include "chrono"
 #include "random_scene.h"
 
-double  hit_sphere(const point3& center, double radius, const ray& ray);
-colour ray_colour(const ray& r, const hittable& world, int depth);
+double hit_sphere(const point3 &center, double radius, const ray &ray);
+
+colour ray_colour(const ray &r, const hittable &world, int depth);
 
 int main() {
     //Image
-    const auto aspect_ratio = 16.0 / 9.0;
-    const int image_width = 400;
-    const int image_height = static_cast<int>(image_width/aspect_ratio);
-    const int samples = 500;
-    const int max_depth = 200;
-
+    const auto aspect_ratio = 1.5;
+    const int image_width = 300;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples = 50;
+    const int max_depth = 50;
 
     //World
-
     auto world = random_scene();
 
     //Camera
-
-    point3 lookfrom(13,2,3);
-    point3 lookat(0,0,0);
-    vec3 vup(0,1,0);
+    point3 lookfrom(13, 2, 3);
+    point3 lookat(0, 0, 0);
+    vec3 vup(0, 1, 0);
     auto dist_to_focus = 10;
 //    auto dist_to_focus = (lookfrom-lookat).length();
     auto aperture = 0.1;
@@ -36,50 +36,64 @@ int main() {
     camera cam(lookfrom, lookat, vup, 20, aspect_ratio, aperture, dist_to_focus);
 
     // Files
-    std::ofstream file ("../Image.ppm");
+    std::ofstream file("../Image.ppm");
+    std::ostringstream oss;
 
     // Rendering
     auto start = std::chrono::steady_clock::now();
-    // pixel data
-    file << "P3\n" << image_width << " " << image_height << "\n255\n";
-    for (int j = image_height-1; j >= 0; --j) {
-        std::cerr  << "\r Rows Remaining: " << j << "\n";
-        for (int i = 0; i < image_width; ++i){
-                colour pixel_colour(0,0,0);
-                for (int s = 0; s < samples; ++s){
-                    auto u = (i + random_double()) / (image_width-1);
-                    auto v = (j + random_double()) / (image_height-1);
-                    ray r = cam.get_ray(u,v);
-                    pixel_colour += ray_colour(r, world, max_depth);
-                }
-                write_colour(file, pixel_colour, samples);
+    // size of each pixel will be 12 bytes as a string
+    // doing everything at once
+    std::string image[image_width][image_height];
+    for (int i = 0; i < image_width; i++) {
+        for (int j = 0; j < image_height; j++) {
+            image[i][j].resize(12);
+            colour pixel_colour(0, 0, 0);
+            for (int s = 0; s < samples; ++s) {
+                auto u = (i + random_double()) / (image_width - 1);
+                auto v = (j + random_double()) / (image_height - 1);
+                ray r = cam.get_ray(u, v);
+                pixel_colour += ray_colour(r, world, max_depth);
             }
+            image[i][j] = write_colour_str(pixel_colour, samples);
         }
-    auto end  = std::chrono::steady_clock::now();
-    std::chrono::duration<double> elapsed = end-start;
+        std::cerr << "\r cols Remaining: " << image_width - i << "\n";
+    }
+
+    // ppm header
+    oss << "P3\n" << image_width << " " << image_height << "\n255\n";
+    for (int i = image_height - 1; i >= 0; --i) {
+        for (int j = 0; j < image_width; j++) {
+            oss << image[j][i] << std::endl;
+        }
+    }
+    file << oss.str();
+
+
+    auto end = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
     std::cerr << "\nRender Complete\n";
-    std::cerr << "Elapsed time: " << elapsed.count() << "s"<< std::endl;
+    std::cerr << "Elapsed time: " << elapsed.count() << "s" << std::endl;
     file.close();
 }
 
 // Determines if a ray would hit the sphere using the discriminant
 // either return -1 if not hit or the positive R where it did hit
-double  hit_sphere(const point3& center, double radius, const ray& ray){
+double hit_sphere(const point3 &center, double radius, const ray &ray) {
     vec3 oc = ray.origin() - center;
     auto a = ray.direction().length_squared();
     auto half_b = dot(oc, ray.direction());
-    auto c = oc.length_squared() -radius*radius;
+    auto c = oc.length_squared() - radius * radius;
     // the discriminant whoa
-    auto discriminant = half_b*half_b - a*c;
-    if (discriminant < 0){
+    auto discriminant = half_b * half_b - a * c;
+    if (discriminant < 0) {
         return -1.0;
-    }else  {
+    } else {
         return (-half_b - sqrt(discriminant) / a);
     }
 
 }
 
-colour ray_colour(const ray& r, const hittable& world, int depth) {
+colour ray_colour(const ray &r, const hittable &world, int depth) {
     hit_record rec;
 
     if (depth <= 0) return colour(0, 0, 0);
